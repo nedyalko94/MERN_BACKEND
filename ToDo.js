fd4510f6ce0d6227bf525a1a2bfa92bd20b495bc
Nedyalko Todorov <105845092+nedyalko94@users.noreply.github.com>
2022-12-29 07:41:51 +0100

    up
    a

---
+++ ToDo.js
@@ -0,0 +1,92 @@
+//@
+//#
+//$
+//%
+//! 
+///
+//?
+//~
+//1
+//2
+//3
+
+
+//# ToDo 
+
+//?  send email with question   
+//@ done 
+//?  send newsletter
+//@ done 
+//?  email  confirmation
+//@ done 
+//?  add to shopping card 
+//@ done 
+//?  add to wishlist 
+//@ done
+
+//? protect route in front end 
+//@ done 
+//? protect route in back end 
+ //@  
+//? payment gateway 
+//@ done 
+
+//? make it responsive 
+//@ done 
+
+//# optional upgrade
+//? vote for product
+//@
+//? calculate and display vote
+//@ 
+//? action 
+//@
+//? promotion 
+//@
+//? trending 
+//@
+
+
+//% ----------------------------------Fixing problems and upgrade options
+
+//% Upgrade
+
+//? check the token deletion adding with password reset
+
+//? comment section
+//@ done still can be upgrade with more options
+    //# rotate the comment last to be display first 
+
+//? add google maps 
+//@ done with warning
+//$  
+
+
+
+
+//? add section with purchase
+//@
+
+//? remember the button after adding in wishlist or card
+//@ done even upgraded to add / remove /remember(check)
+
+
+
+//% ---- ERRORS
+
+
+//! can't log in from diff page 
+//@
+//! on key down should send me to home with result of search 
+//@ done
+//! when there is no product
+//@ done
+//! when get user  get wrong info 
+//@ done 
+//! dashboard style bug
+//@ done
+//! fix the bug in profile when rerender  get error and can't get user 
+//@ done
+//! can't send  multiple email with newsletter block from  the email provider//spam (need email change )
+//@
+
