b95af6e9c86bb9300b4379911cc90a5d23c0d411
nedyalko94 <nedyalko.todorov94@gmail.com>
2022-12-30 05:17:40 +0100

    A

--- app.js
+++ app.js
@@ -58,6 +58,16 @@ app.use(cors({
 
 }))
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://endprojectcybertech.netlify.app/");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(
