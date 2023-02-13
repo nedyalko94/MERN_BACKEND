67868901fe9f7bf905e915fc511909398fbeddd3
nedyalko94 <nedyalko.todorov94@gmail.com>
2023-02-13 16:51:48 +0100

    fix the look

--- ToDo.js
+++ ToDo.js
@@ -67,7 +67,7 @@
     //# rotate the comment last to be display first 
 
 //? add google maps 
-//@ done with warning
+//@ done with some warning warning
 //$  
 
 //? add section with purchase history
@@ -94,6 +94,6 @@
 //! fix the bug in profile when rerender  get error and can't get user 
 //@ done
 //! can't send  multiple email with newsletter block from  the email provider//spam (need email change )
-//@  can't be fix
+//@  can't be fix need new email 
 
 
